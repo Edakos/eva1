96eb12e9062791007620a343651e249fb250706e
Edgar Valarezo <edgar.valarezo@gmail.com>
2012-01-28 14:08:44 -0700

    adding pager

--- htdocs/sites/all/themes/garland/template.php
+++ htdocs/sites/all/themes/garland/template.php
@@ -102,3 +102,30 @@ function phptemplate_get_ie_styles() {
 
   return $iecss;
 }
+
+function phptemplate_pager($tags = array(), $limit = 10, $element = 0, $parameters = array(), $quantity = 9) {
+  global $pager_total;
+  $pager_total[$element] = $quantity;
+
+  $li_previous = theme('pager_previous', (isset($tags[1]) ? $tags[1] : '< anteriores ' . $limit), $limit, $element, 1, $parameters);
+  $li_next = theme('pager_next', (isset($tags[3]) ? $tags[3] : 'siguientes ' . $limit . ' >'), $limit, $element, 1, $parameters);
+
+  if ($pager_total[$element] > 1) {
+  //if ($quantity > 1) {
+
+    if ($li_previous) {
+      $items[] = array(
+        'class' => 'pager-previous', 
+        'data' => $li_previous,
+      );
+    }
+    // End generation.
+    if ($li_next) {
+      $items[] = array(
+        'class' => 'pager-next', 
+        'data' => $li_next,
+      );
+    }
+    return theme('item_list', $items, NULL, 'ul', array('class' => 'pager'));
+  }
+}
